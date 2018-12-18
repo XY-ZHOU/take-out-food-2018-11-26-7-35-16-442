8301e68dfa66511bb82f68bf1ccdee78ec8b2ec3
XY-ZHOU <731986251@qq.com>
2018-12-18 19:52:24 +0800

    put order message in arr

--- src/best-charge.js
+++ src/best-charge.js
@@ -16,3 +16,16 @@ function transformSelectedItems(selectedItems) {
   inputArr.push(countArr);
   return inputArr;
 }
+
+function orderObj(allItemsArr, inputArr) {
+  let allOrderMessageArr = [];
+  for (let i = 0; i < allItemsArr.length; i++) {
+    if (judgeElementInArr(inputArr[0], allItemsArr[i].id)) {
+      let index = inputArr[0].indexOf(allItemsArr[i].id);
+      allItemsArr[i].count = inputArr[1][index];
+      allItemsArr[i].itemSum = inputArr[1][index] * allItemsArr[i].price;
+      allOrderMessageArr.push(allItemsArr[i]);
+    }
+  }
+  return allOrderMessageArr;
+}
