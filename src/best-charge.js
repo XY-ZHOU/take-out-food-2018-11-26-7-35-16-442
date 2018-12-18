c49778241257d174cb7515ebc3119a890bf52a99
XY-ZHOU <731986251@qq.com>
2018-12-18 19:51:06 +0800

    transform selectedItems

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,18 @@
 function bestCharge(selectedItems) {
   return /*TODO*/;
 }
+
+function transformSelectedItems(selectedItems) {
+  let inputArr = [],
+    itemArr = [],
+    countArr = [];
+  for (let i = 0; i < selectedItems.length; i++) {
+    let itemId = selectedItems[i].slice(0, selectedItems[i].indexOf("x") - 1);
+    let count = selectedItems[i].slice(selectedItems[i].indexOf("x") + 1);
+    itemArr.push(itemId);
+    countArr.push(count);
+  }
+  inputArr.push(itemArr);
+  inputArr.push(countArr);
+  return inputArr;
+}
